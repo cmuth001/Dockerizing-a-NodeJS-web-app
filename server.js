36da7108cd23aa5ceac9078e2fb75c74781b70c8
KarthikVadrevu8 <117923769+KarthikVadrevu8@users.noreply.github.com>
2023-01-24 17:18:54 +0000

    Update server.js

--- server.js
+++ server.js
@@ -14,3 +14,16 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+const englishCode = "en-US";
+const spanishCode = "es-ES";
+function getAboutUsLink(language){
+    switch (language.toLowerCase()){
+      case englishCode.toLowerCase():
+        return '/about-us';
+      case spanishCode.toLowerCase():
+        return '/acerca-de';
+    }
+    return '';
+}
+module.exports = getAboutUsLink;
