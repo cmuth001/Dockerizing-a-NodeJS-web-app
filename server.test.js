c04bb8afaac22e8f07f65b9d67628851df90a297
KarthikVadrevu8 <117923769+KarthikVadrevu8@users.noreply.github.com>
2023-01-24 17:32:44 +0000

    Update server.test.js

--- server.test.js
+++ server.test.js
@@ -1,4 +1,4 @@
-
+const app = require("./server");
 const getAboutUsLink = require("./server");
 test("Returns about-us for english language", () => {
     expect(getAboutUsLink("en-US")).toBe("/about-us");
